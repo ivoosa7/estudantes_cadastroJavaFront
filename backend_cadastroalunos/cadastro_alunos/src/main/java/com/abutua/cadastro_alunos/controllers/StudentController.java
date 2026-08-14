7846f57457e66f87bdff90e2712187b0f8c92ff9
ivoosa7 <ivo.souza-23@outlook.com>
2026-08-14 10:03:27 -0300

    feat: implementa endpoint GET /students/{id} (closes #5)

--- backend_cadastroalunos/cadastro_alunos/src/main/java/com/abutua/cadastro_alunos/controllers/StudentController.java
+++ backend_cadastroalunos/cadastro_alunos/src/main/java/com/abutua/cadastro_alunos/controllers/StudentController.java
@@ -3,8 +3,11 @@ package com.abutua.cadastro_alunos.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.abutua.cadastro_alunos.models.Student;
 
@@ -12,6 +15,7 @@ import com.abutua.cadastro_alunos.models.Student;
 public class StudentController {
     //Creating a mock list of students
     private final List<Student> students = Arrays.asList(
+
         new Student(1, "João Silva", "joao.silva@example.com", "123456789", 1, 2),
         new Student(2, "Maria Oliveira", "maria.oliveira@example.com", "987654321", 2, 3),
         new Student(3, "Pedro Santos", "pedro.santos@example.com", "456789123", 3, 1),
@@ -25,4 +29,12 @@ public class StudentController {
         return students;
     }
 
+    // Endpoint to get a specific student by ID
+    @GetMapping("/students/{id}")
+    public Student getStudentById(@PathVariable int id){
+        return students.stream()
+                    .filter(student -> student.getId() == id)
+                    .findFirst()
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Aluno não encontrado!"));
+    }
 }
